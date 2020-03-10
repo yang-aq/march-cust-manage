b1ea6bd5648d851e54a23c6e846801c2b106f0e5
杨岸青 <1059868247@qq.com>
2020-03-10 13:37:09 +0800

    添加VO，修改Feign和controller

--- cust-manage-web/src/main/java/com/pccw/cust/manage/web/controller/AccountController.java
+++ cust-manage-web/src/main/java/com/pccw/cust/manage/web/controller/AccountController.java
@@ -16,7 +16,6 @@ import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.SpringQueryMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
